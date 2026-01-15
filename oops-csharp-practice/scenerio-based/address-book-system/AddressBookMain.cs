4cc286de28007031732a863aafb59bd814b9ea55
hemant12797 <hemantthakur8439@gmail.com>
2026-01-15 12:06:15 +0530

    [Hemant Kumar] added address book scenerio based problem

---
+++ oops-csharp-practice/scenerio-based/address-book-system/AddressBookMain.cs
@@ -0,0 +1,20 @@
+﻿internal class AddressBookMain
+{
+    // Now we support adding multiple address books
+    public static AddressBook[] AddressBooks;
+    public static int AddressBookArrayIndex;
+
+    static AddressBookMain()
+    {
+        // The user can add at most 10 address books.
+        AddressBooks = new AddressBook[10]; 
+        // Initially we add an address book with a max size of 10 and name default.
+        AddressBooks[0] = new AddressBook(10, "default");
+        AddressBookArrayIndex = 1; // As index 0 is occupied by address book named "default"
+    }
+    static void Main(string[] args)
+    {
+        AddressBookMenu addressBookMenu = new AddressBookMenu();
+        addressBookMenu.ShowMenu();
+    }
+}
---
+++ oops-csharp-practice/scenerio-based/address-book-system/Contact.cs
@@ -0,0 +1,99 @@
+﻿internal class Contact
+{
+    private string FirstName;
+    private string LastName;
+    private string City;
+    private string State;
+    private string ZipCode;
+    private string PhoneNumber;
+    private string Email;
+
+    public Contact(string firstName, string lastName, string city, string state, string zipCode, string phoneNumber, string email)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        City = city;
+        State = state;
+        ZipCode = zipCode;
+        PhoneNumber = phoneNumber;
+        Email = email;
+    }
+
+    // Getters and Setter for all the fields
+    public string GetFirstName()
+    {
+        return FirstName;
+    }
+
+    public void SetFirstName(string firstName)
+    {
+        FirstName = firstName;
+    }
+
+    public string GetLastName()
+    {
+        return LastName;
+    }
+    
+    public void SetLastName(string lastName)
+    {
+        LastName = lastName;
+    }
+
+    public string GetCity()
+    {
+        return City;
+    }
+
+    public void SetCity(string city)
+    {
+        City = city;
+    }
+
+    public string GetState()
+    {
+        return State;
+    }
+
+    public void SetState(string state)
+    {
+        State = state;
+    }
+
+    public string GetZipCode()
+    {
+        return ZipCode;
+    }
+
+    public void SetZipCode(string zipCode)
+    {
+        ZipCode = zipCode;
+    }
+
+    public string GetPhoneNumber()
+    {
+        return PhoneNumber;
+    }
+
+    public void SetPhoneNumber(string phoneNumber)
+    {
+        PhoneNumber = phoneNumber;
+    }
+
+    public string GetEmail()
+    {
+        return Email;
+    }
+
+    public void SetEmail(string email)
+    {
+        Email = email;
+    }
+
+    // Overriding ToString to allow printing a Contact
+    public override string ToString()
+    {
+        return $"\nFirst Name: {FirstName} | Last Name: {LastName} | City: {City} | State: {State} | Zip Code: {ZipCode} | Phone Number: {PhoneNumber} | Email: {Email}";
+    }
+    
+}
